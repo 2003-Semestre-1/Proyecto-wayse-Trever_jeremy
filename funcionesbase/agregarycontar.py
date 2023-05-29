721c6a1e7aff245d587f9c72af56a92cdee59f22
Gnarty <58467534+Gnarty@users.noreply.github.com>
2023-05-29 08:18:09 -0600

    Update agregarycontar.py

--- funcionesbase/agregarycontar.py
+++ funcionesbase/agregarycontar.py
@@ -6,3 +6,14 @@ salida: agregar un elemento a una lista
 def append(lista, elemento):
     lista += [elemento]
     return lista
+
+"""
+nombre: Len
+entrada: lista
+salida: contador de indises que existen en una lista
+"""
+def Len(objeto):
+    contador = 0
+    for elemento in objeto:
+        contador += 1
+    return contador
